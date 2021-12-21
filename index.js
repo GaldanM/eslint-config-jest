f2fddbb0b95e47647200386187287f284f9833a2
Galdan Moulinneuf <14978190+GaldanM@users.noreply.github.com>
2021-12-21 22:32:01 +0100

    disable prefer-expect-assertions

--- index.js
+++ index.js
@@ -2,4 +2,7 @@ module.exports = {
   plugins: ["eslint-plugin-jest"],
   extends: ["@galdanm/eslint-config-base", "plugin:jest/all"],
   env: { es6: true, "jest/globals": true },
+  rules: {
+    "jest/prefer-expect-assertions": "off",
+  },
 };
